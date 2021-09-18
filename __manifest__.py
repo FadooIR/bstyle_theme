85fda38d9dafe5e6a82b7bf789af039b783a37f7
Saeed Raeisi <saeed.raesi2020@gmail.com>
2021-09-18 13:11:09 +0430

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'business style theme',
+    'version': '14.0.1.0.1',
+    'summary': 'business style theme',
+    'author': 'fadoo',
+    'license': 'AGPL-3',
+    'maintainer': 'fadoo',
+    'company': 'fadoo',
+    "category": "business style theme/Theme",
+    'website': 'http://fadoo.ir',
+    'depends': ['web'],
+    'description': """
+           business style theme
+    """,
+    'data': [
+    'views/webclient_template_extend.xml',
+    ],
+    'price':0,
+    'currency':'IRR',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png']
+}
